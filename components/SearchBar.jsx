1bf826a29d5033a4d9393947b21c7fd73d4c178e
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-11-29 00:06:09 +0100

    update searchbar shadow

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = ({ input, handleInputChange }) => {
     <Input
       name="full_name"
       type="text"
-      className="p-2 w-40 sm:w-72 fixed top-2.5 right-2 text-gray-800"
+      className="p-2 w-40 sm:w-72 fixed top-2.5 right-2 text-gray-800 shadow-lg"
       value={input}
       onChange={handleInputChange}
       placeholder="Type to search"
